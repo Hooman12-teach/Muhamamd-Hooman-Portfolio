654ad358b52c2b8749bb7fc87327a6b088258af3
Hooman12-teach <hoomankhan701@gmail.com>
2025-12-03 11:18:42 +0500

    Add files via upload

---
+++ cv project of my/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".hooman12", {
+        strings: ["Frontend Developer", "YouTuber", "Explorer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
